09d1aa3436af0540cd8a476aabc375eaef2ad959
Tuyen Huynh <hntuyen282@gmail.com>
2025-07-11 00:00:29 -0400

    11ty setup

---
+++ eleventy.config.mjs
@@ -0,0 +1,29 @@
+import { pathToFileURL } from "node:url";
+import { evaluate } from "@mdx-js/mdx";
+import { renderToStaticMarkup } from "react-dom/server";
+import * as runtime from "react/jsx-runtime";
+import postcssPlugin from "@jgarber/eleventy-plugin-postcss";
+
+export default async function (eleventyConfig) {
+    // Plugins
+    eleventyConfig.addPlugin(postcssPlugin);
+
+    // Passthrough copy font(s)
+    // eleventyConfig.addPassthroughCopy("assets/fonts");
+
+    // MDX support
+    eleventyConfig.addExtension("mdx", {
+        compile: async (str, inputPath) => {
+            const { default: mdxContent } = await evaluate(str, {
+                ...runtime,
+                baseUrl: pathToFileURL(inputPath)
+            });
+
+            return async function (data) {
+                let res = await mdxContent(data);
+                return renderToStaticMarkup(res);
+            }
+        }
+    });
+    eleventyConfig.addTemplateFormats("mdx")
+};
